04c1fe2bdce77b73076e7a128f71703652b27b3e
zeinaboud <zeinaboudaimah@gmail.com>
2025-07-03 14:44:49 +0300

    update json file

--- src/views/detailproduct.vue
+++ src/views/detailproduct.vue
@@ -15,9 +15,19 @@ const toast = useToast();
 const product = computed(() =>
     store.products.find(p => p.id == route.params.id)
 );
+// If products not loaded yet, fetch them
+if (!store.products.length) {
+  store.fetchProducts();
+}
+
+// Compute full image URL using BASE_URL (for GitHub Pages)
+const imageUrl = computed(() => {
+  return product.value ? import.meta.env.BASE_URL + product.value.image : '';
+});
+
 const handleAdd =()=>{
     cart.addToCart(product.value);
-    toast.success(`${product.name} added to cart! 🛒`);
+    toast.success(`${product.value.name} added to cart! 🛒`);
     /*router.push('/cart');*/
 }
 </script>
@@ -25,11 +35,13 @@ const handleAdd =()=>{
     <div v-if="product">
         <div class=" max-w-7xl  mw-auto flex  flex-col lg:flex-row  gap-10">
             <div class="text-center lg:w-1/2">
-                <img :src="product.image" alt="product.name" class="mx-auto mt-6  sm:px-6 lg:gap-8 lg:px-8">
+                <img :src="imageUrl" :alt="product.name" class="mx-auto mt-6  sm:px-6 lg:gap-8 lg:px-8">
             </div>
             <div class="w-full lg:w-1/2 flex flex-col md:mt-8">
                 <div class="flex items-center mb-2">
-                    <Icon v-for="i in 5" :key="i" icon="line-md:star-alt-filled" width="16" height="16" 
+                    <Icon v-for="i in 5" 
+                    :key="i" 
+                    icon="line-md:star-alt-filled" width="16" height="16" 
                     :class="i <= Math.floor(product.rating) ? 'text-yellow-400' : 'text-gray-300'"></Icon>
                     <span class="text-sm text-blue-500 ml-3">{{product.rating}} reviews</span>
                 </div>
